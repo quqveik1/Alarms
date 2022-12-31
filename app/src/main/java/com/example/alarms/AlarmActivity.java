d159eee6c2e9ac2a4522ce88139a11f9d7bae5b5
Alex Bogomol <quqveik.1@gmail.com>
2022-12-31 21:25:14 +0300

    Timer works

--- app/src/main/java/com/example/alarms/AlarmActivity.java
+++ app/src/main/java/com/example/alarms/AlarmActivity.java
@@ -1,12 +1,62 @@
 package com.example.alarms;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+import android.widget.Toast;
 
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
 public class AlarmActivity extends AppCompatActivity {
-    protected void onCreate(Bundle savedInstanceState) {
+    TextView timeView;
+    Button okButton;
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_alarm);
+        Toast.makeText(getApplicationContext(), "Таймер сработал", Toast.LENGTH_LONG).show();
+
+        timeView = findViewById(R.id.timeView);
+        okButton = findViewById(R.id.okButton);
+        okButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                finish();
+            }
+        });
+        Intent previousIntent = getIntent();
+
+        long totalTime = 0;
+
+        try {
+            totalTime = previousIntent.getLongExtra("Total time", 0);
+        }
+        catch (Exception e)
+        {
+            Toast.makeText(getApplicationContext(), "Проблема с получением времени таймера:  " + e.toString(), Toast.LENGTH_LONG).show();    
+        }
+        long hours = totalTime / (3600 * 1000);
+        long minutes = (totalTime -(hours * 3600 * 1000)) / (60 * 1000);
+        long seconds = (totalTime - (hours * 3600 * 1000) - (minutes * 60 * 1000)) / 1000;
+
+        String outputStr = "";
+        if (hours != 0)
+        {
+            outputStr += hours + " часов ";
+        }
+        if (minutes != 0)
+        {
+            outputStr += minutes + " минут ";
+        }
+        if (seconds != 0)
+        {
+            outputStr += seconds + " cекунд ";
+        }
+        timeView.setText(outputStr);
+
     }
 }
--- app/src/main/java/com/example/alarms/MainActivity.java
+++ app/src/main/java/com/example/alarms/MainActivity.java
@@ -1,27 +1,249 @@
 package com.example.alarms;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
+import androidx.core.content.ContextCompat;
 
+import android.Manifest;
 import android.app.AlarmManager;
 import android.app.PendingIntent;
-import android.content.Context;
 import android.content.Intent;
+import android.content.pm.PackageManager;
 import android.os.Bundle;
+import android.provider.Settings;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
-    private AlarmService alarmService = new AlarmService();
+
+    private Button timerStartButton;
+    private EditText editViewHours;
+    private EditText editViewMinutes;
+    private EditText editViewSeconds;
+
+    private static boolean canSCHEDULE_EXACT_ALARM = true;
+    //private static final int SCHEDULE_EXACT_ALARM = 57;
+    private static boolean canUSE_EXACT_ALARM = true;
+    //private static final int USE_EXACT_ALARM = 1329;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        makeAlarm();
+        /*
+
+        requestPermissionLauncher =
+                registerForActivityResult(new ActivityResultContracts.RequestPermission(), new ActivityResultCallback<Boolean>() {
+                    @Override
+                    public void onActivityResult(Boolean result) {
+                        if (result)
+                        {
+                            SCHEDULE_EXACT_ALARM = true;
+                        }
+
+                    }
+                });
+
+         */
+
+        timerStartButton = findViewById(R.id.timerStartButton);
+        editViewHours = findViewById(R.id.editViewHours);
+        editViewMinutes = findViewById(R.id.editViewMinutes);
+        editViewSeconds = findViewById(R.id.editViewSeconds);
+
+
+        timerStartButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                try {
+                    long timeInMillis = 0;
+                    long hours = 0;
+                    long minutes = 0;
+                    long seconds = 0;
+
+                    if (!editViewHours.getText().toString().isEmpty())
+                        hours = Integer.parseInt(editViewHours.getText().toString());
+                    if (!editViewMinutes.getText().toString().isEmpty())
+                        minutes = Integer.parseInt(editViewMinutes.getText().toString());
+                    if (!editViewSeconds.getText().toString().isEmpty())
+                        seconds = Integer.parseInt(editViewSeconds.getText().toString());
+
+                    timeInMillis = (hours * 3600 + minutes * 60 + seconds) * 1000;
+                    makeTimer(timeInMillis);
+                } catch (Exception e) {
+                    Toast.makeText(getApplicationContext(), "Ошибка постановка таймера: " + e.toString(), Toast.LENGTH_LONG).show();
+                }
+
+            }
+        });
+        //makeTimer(1000);
+
     }
+    /*
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode,
+                                           @NonNull String[] permissions,
+                                           @NonNull int[] grantResults)
+    {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+
+        if (requestCode == SCHEDULE_EXACT_ALARM)
+        {
+            if (!(grantResults.length > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED))
+            {
+                Toast.makeText(getApplicationContext(), "Программа работать не будет", Toast.LENGTH_LONG).show();
+            }
+            else
+            {
+                canSCHEDULE_EXACT_ALARM = true;
+            }
+        }
+
+        if (requestCode == USE_EXACT_ALARM)
+        {
+
+            if (!(grantResults.length > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED)) {
+                Toast.makeText(getApplicationContext(), "Программа работать не будет", Toast.LENGTH_LONG).show();
+            } else {
+                canUSE_EXACT_ALARM = true;
+            }
+
+        }
+    }
+    */
+    /*
+    private static final int WRITE_STORAGE = 57;
+    private static final int READ_STORAGE = 1329;
+
+    private void checkPermission()
+    {
+        boolean write =
+                ContextCompat.checkSelfPermission(
+                        this,
+                        Manifest.permission.WRITE_EXTERNAL_STORAGE
+                ) == PackageManager.PERMISSION_GRANTED;
+        boolean read =
+                ContextCompat.checkSelfPermission(
+                        this,
+                        Manifest.permission.READ_EXTERNAL_STORAGE
+                ) == PackageManager.PERMISSION_GRANTED;
+
+        if (write == false)
+        {
+            ActivityCompat.requestPermissions(MainActivity.this, new String[] { Manifest.permission.WRITE_EXTERNAL_STORAGE }, WRITE_STORAGE);
+        }
+
+        if (read == false)
+        {
+            ActivityCompat.requestPermissions(MainActivity.this, new String[] { Manifest.permission.READ_EXTERNAL_STORAGE }, READ_STORAGE);
+        }
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode,
+                                           @NonNull String[] permissions,
+                                           @NonNull int[] grantResults)
+    {
+        super.onRequestPermissionsResult(requestCode,
+                permissions,
+                grantResults);
+
+        if (requestCode == WRITE_STORAGE)
+        {
+            if (!(grantResults.length > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED))
+            {
+                Toast toast = Toast.makeText(this, "ДЛЯ РАБОТЫ НЕОБХОДИМ ДОСТУП К ПАМЯТИ!!!", Toast.LENGTH_SHORT);
+                toast.show();
+            }
+        }
+
+
+    };
+     */
+
+
+
+    private void makeTimer(long timeInMillis)
+    {
+        AlarmManager alarmManager = (AlarmManager)getSystemService(ALARM_SERVICE);
+        long curTime = System.currentTimeMillis();
+        //checkPermission();
+        if (canSCHEDULE_EXACT_ALARM) {
+            try {
+                Intent alarmIntent = new Intent(this, AlarmActivity.class);
+                alarmIntent.putExtra("Total time", timeInMillis);
+                PendingIntent pendingIntent = PendingIntent.getActivity(getApplicationContext(), 0, alarmIntent, PendingIntent.FLAG_UPDATE_CURRENT  |  PendingIntent.FLAG_MUTABLE);
+                alarmManager.setExactAndAllowWhileIdle(AlarmManager.RTC_WAKEUP, curTime + timeInMillis, pendingIntent);
+                Toast.makeText(getApplicationContext(), "Таймер поставлен", Toast.LENGTH_LONG).show();
+            } catch (Exception e) {
+                Toast.makeText(getApplicationContext(), "Ошибка постановка таймера: " + e.toString(), Toast.LENGTH_LONG).show();
+            }
+        }
+        else
+        {
+            Toast.makeText(getApplicationContext(), "Нет необходимого разрешения", Toast.LENGTH_LONG).show();
+
+        }
+    }
+
+
+    /*
+    private boolean checkPermission()
+    {
+        canSCHEDULE_EXACT_ALARM = (ContextCompat.checkSelfPermission(this, Manifest.permission.SCHEDULE_EXACT_ALARM) == PackageManager.PERMISSION_GRANTED);
+        if (canSCHEDULE_EXACT_ALARM) return true;
+        else
+        {
+            try {
+                ActivityCompat.requestPermissions(MainActivity.this, new String[] { Manifest.permission.SCHEDULE_EXACT_ALARM }, SCHEDULE_EXACT_ALARM);
+                /*
+                Intent intent = new Intent();
+                intent.setAction(Settings.ACTION_REQUEST_SCHEDULE_EXACT_ALARM);
+                intent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK | Intent.FLAG_ACTIVITY_CLEAR_TOP);
+                getApplicationContext().startActivity(intent);
+
+
+            }
+            catch (Exception e)
+            {
+                Toast.makeText(getApplicationContext(), "Ошибка постановка таймера: " + e.toString(), Toast.LENGTH_LONG).show();
+            }
+
+        }
+
+
+
+        canUSE_EXACT_ALARM = (ContextCompat.checkSelfPermission(this, Manifest.permission.READ_EXTERNAL_STORAGE) == PackageManager.PERMISSION_GRANTED);
+        if (canUSE_EXACT_ALARM) return true;
+        else
+        {
+            try {
+                ActivityCompat.requestPermissions(MainActivity.this, new String[] { Manifest.permission.READ_EXTERNAL_STORAGE }, USE_EXACT_ALARM);
+
+            }
+            catch (Exception e)
+            {
+                Toast.makeText(getApplicationContext(), "Ошибка постановка таймера: " + e.toString(), Toast.LENGTH_LONG).show();
+            }
+
+        }
+
+        return false;
+    }
+
+     */
+
+
 
     private void makeAlarm()
     {
 
+        /*
         Intent intent = new Intent(this, MainActivity.class);
         intent.setFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP | Intent.FLAG_ACTIVITY_NEW_TASK);
         //intent.putExtra(AS_TIMER, data);
@@ -58,5 +280,6 @@ public class MainActivity extends AppCompatActivity {
 
 
         //am.setAlarmClock(alarmClockInfo, );
+         */
     }
 }
